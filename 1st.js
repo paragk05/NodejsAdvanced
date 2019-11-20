aca52b0e306e4ef106b00b87ede0f8bf055f92fc
Paragk05 <paragk106@gmail.com>
2019-11-20 13:39:36 +0530

    Mysql Connection with nodejs

--- 1st.js
+++ 1st.js
@@ -2,16 +2,48 @@ const express=require('express');
 
 const app=express();
 
+const mysql= require('mysql');
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    database: 'Nodejs',
+    user: 'root',
+    password:'Parag@765'
+});
+
 app.get("/",(req,res)=>
 {
-    res.send('Responding..')
+    res.send('Responding..');
 
-})
+});
 
 app.get("/user",(req,res)=>
 {
-    res.send("Response from user..");
-})
+    const inputQuery="select * from user";
+    const userId= req.params.id;
+    connection.query(inputQuery,(err,row,field)=>
+    {
+        if(err)
+        console.log(err);
+        else
+        res.json(row);
+    })
+});
+
+app.get("/user/:id",(req,res)=>
+{
+    const inputQuery="select * from user where id= ?";
+    const userId= req.params.id;
+    connection.query(inputQuery,[userId],(err,row,field)=>
+    {
+        if(err)
+        console.log(err);
+        else
+        res.json(row);
+    })
+   
+
+});
 
 app.listen(3002,()=>
 {
